546eee69fb33d1eaf632ad2b0cac041cd6cd52f6
jitka <ji.knizkova@gmail.com>
2026-02-11 22:14:05 +0100

    vytvoření hlavní lišty, definice prvního příkazu

--- projekt1.py
+++ projekt1.py
@@ -3,3 +3,16 @@ print("1. Přidat nový úkol")
 print ("2. Zobrazit úkoly ")
 print ("3. Odstranit úkol")
 print("4. Konec programu")
+
+akce_uzivatele = input ("Vyberte číslo od 1 do 4")
+
+if akce_uzivatele == "1":
+   print("ahoj")
+   def pridat_ukol():
+        nazev_ukolu = input ("Název úkolu:")
+        popis_ukolu = input ("Popis úkolu?")
+        print("Úkol", nazev_ukolu, "byl pridan")
+        return 
+   print (pridat_ukol)
+else:
+    print ("konec")
