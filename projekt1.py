34747c6b6ee8a7b911157225f99fea04a86606f4
jitka <ji.knizkova@gmail.com>
2026-02-14 17:30:19 +0100

    jk

--- projekt1.py
+++ projekt1.py
@@ -4,15 +4,15 @@ print ("2. Zobrazit úkoly ")
 print ("3. Odstranit úkol")
 print("4. Konec programu")
 
+ukoly = []
+
 akce_uzivatele = input ("Vyberte číslo od 1 do 4")
 
 if akce_uzivatele == "1":
-   print("ahoj")
-   def pridat_ukol():
-        nazev_ukolu = input ("Název úkolu:")
-        popis_ukolu = input ("Popis úkolu?")
-        print("Úkol", nazev_ukolu, "byl pridan")
-        return 
-   print (pridat_ukol)
+    print("ahoj")
+    nazev_ukolu = input ("vložte název úkolu")
+    def pridat_ukol(nazev_ukolu):
+        return ukoly.append(nazev_ukolu)
+    print (ukoly)
 else:
     print ("konec")
