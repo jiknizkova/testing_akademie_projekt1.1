13d36a9ed59227092f7588a3a8349922e694feb2
jitka <ji.knizkova@gmail.com>
2026-02-15 20:34:23 +0100

    zmeny v definicich funkci

--- projekt1.py
+++ projekt1.py
@@ -1,18 +1,27 @@
-print("Správce úkolů - Hlavní menu")
-print("1. Přidat nový úkol")
-print ("2. Zobrazit úkoly ")
-print ("3. Odstranit úkol")
-print("4. Konec programu")
-
 ukoly = []
 
+
+def hlavni_menu():
+    print("Správce úkolů - Hlavní menu")
+    print("1. Přidat nový úkol")
+    print ("2. Zobrazit úkoly ")
+    print ("3. Odstranit úkol")
+    print("4. Konec programu")
+
+def pridat_ukol():
+    nazev_ukolu = input ("vložte název úkolu: ")
+    popis_ukolu = input ("vložte popis úkolu: ")
+    ukoly.append(nazev_ukolu)
+    print ("přidali jste úkol", nazev_ukolu)
+
+hlavni_menu()
 akce_uzivatele = input ("Vyberte číslo od 1 do 4")
 
 if akce_uzivatele == "1":
-    print("ahoj")
-    nazev_ukolu = input ("vložte název úkolu")
-    def pridat_ukol(nazev_ukolu):
-        return ukoly.append(nazev_ukolu)
-    print (ukoly)
+    pridat_ukol()
+
+
+
 else:
-    print ("konec")
+    print ("Ajaj, neplatná volba, zkuste prosím jinou:)")
+    hlavni_menu()
