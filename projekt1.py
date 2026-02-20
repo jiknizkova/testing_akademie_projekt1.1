32547e53ca8fb8f8297314add097ddb40fbf5672
jitka <ji.knizkova@gmail.com>
2026-02-20 21:27:00 +0100

    vychytání chyb především v přidávání a odstraňování úkolů

--- projekt1.py
+++ projekt1.py
@@ -10,29 +10,29 @@ def hlavni_menu():
 
 def pridat_ukol():
     while True:
-        nazev_ukolu = input ("vložte název úkolu: ")
+        nazev_ukolu = input ("Zadejte název úkolu: ")
         if nazev_ukolu == "":
             pokyn = input("Jejda, nezdali jste nic. Pokud se raději chccete vrátit do menu, stisknete kláves 'm' a enter."
             "Stiskem jakékoliv jiné klávesy se vrátíte zpět k možnosti zadat název úkolu. ")
             if pokyn == "m":
                 break
         else:
-            popis_ukolu = input ("vložte popis úkolu: ")
+            popis_ukolu = input ("Zadejte popis úkolu: ")
             if popis_ukolu == "":
                 pokyn = input("Jejda, nezdali jste nic. Pokud se raději chccete vrátit do menu, stisknete kláves 'm' a enter. "
                 "Stiskem jakékoliv jiné klávesy se vrátíte zpět k možnosti zadání názvu a popisu úkolu. ")
                 if pokyn == "m":
                     break
             else:
-                ukoly.append(f"{nazev_ukolu} - {popis_ukolu}")
-                print ("přidali jste úkol", nazev_ukolu)
+                ukoly.append({"nazev": nazev_ukolu, "popis": popis_ukolu})
+                print (f"Úkol '{nazev_ukolu}' byl přidán")
                 print()
                 break
 
 def zobrazit_ukoly():
     if len(ukoly)>0:
         for polozka in ukoly:
-            print(f"{ukoly.index(polozka) + 1}. úkol: {polozka}")
+            print(f"{ukoly.index(polozka) + 1}. úkol: {polozka['nazev']} - {polozka['popis']}")
         print()
     else:
         print("Ještě nemáte zadané žádné úkoly.")
@@ -41,21 +41,33 @@ def zobrazit_ukoly():
 def odstranit_ukol():
     while True:
         zobrazit_ukoly()
-        cislo_ukolu = input("Zadejte číslo úkolu, který chcete smazat: ")
-        cislo_ukolu = int(cislo_ukolu)
-        skutecne_poradi = cislo_ukolu - 1
-        pocet_ukolu = len(ukoly)
-        if cislo_ukolu > pocet_ukolu:
-            print ("Ups, tenhle úkol v seznamu nemáme. Zkuste to znovu")
-            rozhodnuti = input("Pokud se raději chcete vrátit zpět do menu, stisknete klávesu 'm' a enter. ")
-            if rozhodnuti == "m":
-                break
-        else:
-            ukoly.pop(skutecne_poradi)
-            print("Úkol č. {cislo_ukolu} byl smazán.")
-            print()
+        if ukoly == []:
             break
-        
+        else:
+            
+                try:
+                    cislo_ukolu = input("Zadejte číslo úkolu, který chcete smazat: ")
+                    cislo_ukolu = int(cislo_ukolu)
+                    skutecne_poradi = cislo_ukolu - 1
+                    pocet_ukolu = len(ukoly)
+
+                    if cislo_ukolu <= pocet_ukolu and cislo_ukolu > 0:
+                        smazany_ukol = ukoly.pop(skutecne_poradi)
+                        print(f"Úkol {smazany_ukol['nazev']} byl smazán.")
+                        print()
+                        break
+            
+                    else:
+                        print ("Ups, tenhle úkol v seznamu nemáme. Zkuste to znovu")
+                        rozhodnuti = input("Pokud se raději chcete vrátit zpět do menu, stisknete klávesu 'm' a enter."
+                        "Stiskem jakékoliv jiné klávesy se dostanete zppět k výběru úkolů. ")
+                        if rozhodnuti == "m":
+                            break
+                    
+                except ValueError:
+                    print ("Jej, zadali jste neplatnou hodnotu, zkuste to znovu")
+
+               
 
 
 while True:
